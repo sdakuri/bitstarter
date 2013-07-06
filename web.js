dbca76b4d6d529347864f2b956e9f88d91878195
sdakuri <shashi.dakuri@gmail.com>
2013-07-06 08:01:18 +0000

    bug fix

--- web.js
+++ web.js
@@ -6,7 +6,6 @@ app.get('/', function(request, response) {
     var buffer = fs.readFileSync('index.html');
 	response.send(buffer.toString());
 });
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
