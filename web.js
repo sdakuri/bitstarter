1e2113b603b00de347e6451d9409afd88ab23283
sdakuri <shashi.dakuri@gmail.com>
2013-07-06 07:26:37 +0000

    Updated web.js to pull data from index.html

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readFile('~/assignment/bitstarter/index.html', function(err, data){
+	if(err) throw err;
+	var buffer = new Buffer (data, "utf-8");
+	response.send(buffer.toString('utf8',0,buffer.length));
+}
+
 });
 
 var port = process.env.PORT || 5000;
